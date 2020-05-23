b67032fe5fa201ca5ca323ffa3537e8816101683
Nikhil Sharma <nikhil19@somaiya.edu>
2020-05-24 01:21:03 +0530

    add companies type and link it to user

--- schema/schema.js
+++ schema/schema.js
@@ -9,10 +9,24 @@ const UserType  = new GraphQLObjectType({
         id:  {type: GraphQLString},
         firstName: {type: GraphQLString},
         lastName: {type: GraphQLString},
-        age: {type: GraphQLInt} 
+        age: {type: GraphQLInt},
+        company: {
+            type: CompanyType,
+            resolve(parentValue,args){
+                return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`).then(res => res.data);
+            }
+        }
     })
 })
 
+const CompanyType =  new GraphQLObjectType({
+    name: 'Company',
+    fields: ()=>({
+        id: {type: GraphQLString},
+        name: {type: GraphQLString},
+        description: {type: GraphQLString}
+    })
+})
 
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
